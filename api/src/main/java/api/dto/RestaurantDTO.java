9808e505fbc3de948023265e7faa60a8387ae153
Gustavo Brancaglione <gustavo.brancaglione@gmail.com>
2020-11-07 16:18:09 +0100

    Removing null fields

--- api/src/main/java/api/dto/RestaurantDTO.java
+++ api/src/main/java/api/dto/RestaurantDTO.java
@@ -1,7 +1,11 @@
 package api.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import java.util.List;
 
+@JsonInclude(Include.NON_NULL)
 public class RestaurantDTO {
 
     private long id;
