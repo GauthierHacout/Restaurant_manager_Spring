6b4b3d2508efcba6686f9260788c5d79236e1b91
Gustavo Brancaglione <gustavo.brancaglione@gmail.com>
2020-11-07 15:40:02 +0100

    Creating DTOs for our entities

---
+++ api/src/main/java/api/dto/RestaurantDTO.java
@@ -0,0 +1,36 @@
+package api.dto;
+
+import java.util.List;
+
+public class RestaurantDTO {
+
+    private long id;
+
+    private String name;
+
+    private List<TableDTO> tables;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<TableDTO> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<TableDTO> tables) {
+        this.tables = tables;
+    }
+}
---
+++ api/src/main/java/api/dto/TableDTO.java
@@ -0,0 +1,52 @@
+package api.dto;
+
+import java.util.List;
+
+public class TableDTO {
+
+    private long id;
+
+    private long number;
+
+    private boolean occupied;
+
+    private List<OrderDTO> orders;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public List<OrderDTO> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<OrderDTO> orders) {
+        this.orders = orders;
+    }
+
+    public TableDTO(long id, long number, boolean occupied) {
+        this.id = id;
+        this.number = number;
+        this.occupied = occupied;
+    }
+}
