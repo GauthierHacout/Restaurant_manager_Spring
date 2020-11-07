22fe930055d857dea4a1c364ad2ac73f60cfd559
Gustavo Brancaglione <gustavo.brancaglione@gmail.com>
2020-11-07 15:41:12 +0100

    Creating table controller and building json structure for it
    Creating table controller and

--- core/src/main/java/core/repository/OrderItemRepository.java
+++ core/src/main/java/core/repository/OrderItemRepository.java
@@ -1,6 +1,13 @@
 package core.repository;
 
+import core.model.Order;
 import core.model.OrderItem;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderItemRepository extends GenericRepository<OrderItem> {
+
+    List<OrderItem> findByOrderId(Long id);
 }
